a3b7bb693af2267baf08e1ff0e42215c9c2db0f9
bethelhall <bethelroger33@mail.com>
2020-02-14 12:00:01 +0300

    initial commit

---
+++ cmake-build-debug/CMakeFiles/DBR.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/bethe/CLionProjects/DBR/main.cpp" "C:/Users/bethe/CLionProjects/DBR/cmake-build-debug/CMakeFiles/DBR.dir/main.cpp.obj"
+  "C:/Users/bethe/CLionProjects/DBR/operations.cpp" "C:/Users/bethe/CLionProjects/DBR/cmake-build-debug/CMakeFiles/DBR.dir/operations.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/DBR.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DBR.dir/main.cpp.obj"
+  "CMakeFiles/DBR.dir/operations.cpp.obj"
+  "DBR.pdb"
+  "DBR.exe"
+  "DBR.exe.manifest"
+  "libDBR.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DBR.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
